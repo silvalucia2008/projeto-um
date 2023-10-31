0dae0d7ac7f7689f5db389c342420943a7048f5f
silvalucia2008 <148897228+silvalucia2008@users.noreply.github.com>
2023-10-31 13:55:16 -0300

    adicionando arquivos html.js e css

---
+++ style.css
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(400, 400);
+   background("purple");
+    ;}
+  function draw() {
+    stroke("white")
+    fill("black")
+    
+    if(mouseIsPressed)
+      rect(mouseX,mouseY,20,20);}
